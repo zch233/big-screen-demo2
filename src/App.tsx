041eea735ba491b1ac5e3eb2e97e44d95001797b
zhangchao <529743595@qq.com>
2021-05-11 15:26:47 +0800

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.scss'
 import { useInitSize } from './hooks/useInitSize';
+import header from './assets/header.9d7a7634.png'
 
 function App() {
   const Wrapper = useInitSize()
@@ -8,12 +9,15 @@ function App() {
     <div className="App">
       <Wrapper>
         <>
+          <div className={'content-header'}>
+            <img src={header} alt=""/>
+          </div>
           <div className={'content-main'}>
-            <section className={'section1'}>1</section>
-            <section className={'section2'}>2</section>
-            <section className={'section3'}>3</section>
-            <section className={'section4'}>4</section>
-            <section className={'section5'}>5</section>
+            <section className={'border section1'}>1</section>
+            <section className={'border section2'}>2</section>
+            <section className={'border section3'}>3</section>
+            <section className={'border section4'}>4</section>
+            <section className={'border section5'}>5</section>
           </div>
           <div className={'content-footer'}>2222</div>
         </>
